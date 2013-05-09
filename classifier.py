38a0b153222a5686628e1ce42c3b2111328e06e2
Paul Miller <pmiller987@gmail.com>
2013-05-09 01:16:51 +0000

    Weighted vote with predict_proba

--- classifier.py
+++ classifier.py
@@ -15,20 +15,66 @@ class Classifier(object):
 
     @classmethod
     def preds(self, data, targets, cv, extra):
+        models, weights = self.train(data, targets, cv, extra)
+        preds = self.vote(models, cv, weights)
+        return preds
+
+    @classmethod
+    def train(self, data, targets, cv, extra):
+        models = []
+        weights = []
         #model = GMM(n_components=9, covariance_type="spherical", n_iter=100)
         #model.means_ = np.array([np.array(data[targets == (i+1)]).mean(axis=0) for i in xrange(9)])
         #model.means = np.array([
-        model = Cluster(data, extra, targets)
-        #model = SVC(C=10, tol=0.01, degree=2, class_weight='auto', gamma=0.)
         #model = SGDClassifier(alpha=1., epsilon=0.1, n_iter=10, penalty='l2', power_t=1.)
-        #model = LogisticRegression(C=1, penalty='l1', tol=0.01) # best performance around 0.4 without autoencoding
-        #model = ExtraTreesClassifier()
-        #model = RandomForestClassifier()
-        #model = KNeighborsClassifier()
-        #model = GradientBoostingClassifier()
+        #model = SVC(C=10, tol=0.01, degree=2, class_weight='auto', gamma=0.)
+
+        #models.append(Cluster(data, extra, targets))
+        #weights.append(1.)
+
+        #weights.append(1.)
+        #models.append(model)
+
+        models.append(LogisticRegression(C=1.3, penalty='l1', tol=0.05)) # best performance around 0.4 without autoencoding
+        weights.append(1.)
+
+        #models.append(ExtraTreesClassifier())
+        #weights.append(1.)
+
+        #models.append(RandomForestClassifier())
+        #weights.append(1.)
+
+        #models.append(KNeighborsClassifier())
+        #weights.append(1.0)
+
+        #models.append(GradientBoostingClassifier())
+        #weights.append(1.0)
+
         #model = Net(data, targets)
         #model = DeepNetClassifier(data, targets, epochs=200, smoothing=20, new=True)
-        print "Model: ", model
-        model.fit(data, targets)
-        preds = [model.predict(c) for c in cv]
+        for m in models:
+            m.fit(data, targets)
+        return models, weights
+
+    @classmethod
+    def vote(self, models, cv, weights, debug=False):
+        preds = []
+        for c in cv:
+            probs = []
+            for m in models:
+                p = m.predict_proba(c)[0]
+                if debug: print "Preds from ", m, p
+                probs.append(p)
+            votes = np.zeros(len(probs[0]))
+            for i,p in enumerate(probs):
+                w = weights[i]
+                weighted_pred = p * w
+                votes = votes + weighted_pred
+                if debug: print "weighted preds ", weighted_pred
+            if debug: print "FINAL ", votes, "\n"
+            votes = list(votes)
+            _max = max(votes)
+            p = votes.index(_max) + 1
+            p = str(p) + '.0'
+            preds.append(p)
         return preds
